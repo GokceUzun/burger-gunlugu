2e9c704cabaa99ab7e27f12092a2610d1c88049f
GokceUzun <91784437+GokceUzun@users.noreply.github.com>
2025-10-26 00:02:11 +0300

    Add files via upload

--- app3.py
+++ app3.py
@@ -119,7 +119,7 @@ gokce_yorum = st.text_area("Gökçe Yorumu", key="gokce_yorum")
 gokce_toplam = avg(gokce_scores.values())
 
 ortalama = avg([gokce_toplam, baba_toplam])
-st.info(f"Genel Ortalama: **{ortalama}**⭐  |  Baba: {baba_toplam}⭐ • Gökçe: {gokce_toplam}⭐")
+st.info(f"Genel Ortalama: **{ortalama}** ⭐  |  Baba: {baba_toplam} ⭐ • Gökçe: {gokce_toplam} ⭐")
 
 if st.button("Kaydı Ekle"):
     if not mekan.strip():
